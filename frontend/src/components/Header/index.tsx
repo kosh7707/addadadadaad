eb92823e63b5ec8f267515355c57717d13bfd2fe
llddang <bodyness@naver.com>
2024-06-03 02:59:21 +0900

    feat: header UI 및 로직 구현

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 import { VscSignIn, VscSignOut } from 'react-icons/vsc';
-import { HEADER_HEIGHT } from '../../constants';
+import { FONT_SIZE, HEADER_HEIGHT, PAGE_MAX_WIDTH } from '../../constants';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { logOut } from '../../store/auth.slice';
+import { resetDiaryList } from '../../store/diaryList.slice';
+import { resetFollowedList } from '../../store/followed.slice';
+import { resetFollowingList } from '../../store/following.slice';
 
-const HeaderWrapper = styled.div`
+export const HeaderWrapper = styled.div`
   width: 100vw;
   height: ${HEADER_HEIGHT};
   position: fixed;
@@ -11,8 +16,51 @@ const HeaderWrapper = styled.div`
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
 `;
 
+export const HeaderLayout = styled.div`
+  width: 100%;
+  max-width: ${PAGE_MAX_WIDTH};
+  height: ${HEADER_HEIGHT};
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const StyeldA = styled.a`
+  color: inherit;
+  text-decoration: none;
+`;
+
 const Header = () => {
-  return <HeaderWrapper>Header</HeaderWrapper>;
+  const dispatch = useAppDispatch();
+  const auth = useAppSelector((state) => state.auth).value;
+
+  console.log(auth);
+
+  const handleSignOutClick = () => {
+    dispatch(logOut());
+    dispatch(resetDiaryList());
+    dispatch(resetFollowedList());
+    dispatch(resetFollowingList());
+  };
+
+  return (
+    <HeaderWrapper>
+      <HeaderLayout>
+        <StyeldA href="/" style={{ fontSize: FONT_SIZE.lg, lineHeight: HEADER_HEIGHT }}>
+          Diary
+        </StyeldA>
+        {auth.isAuth ? (
+          <StyeldA href="/" onClick={handleSignOutClick}>
+            <VscSignOut style={{ fontSize: FONT_SIZE.lg, marginTop: '6px' }} />
+          </StyeldA>
+        ) : (
+          <StyeldA href="/sign-in">
+            <VscSignIn style={{ fontSize: FONT_SIZE.lg, marginTop: '6px' }} />
+          </StyeldA>
+        )}
+      </HeaderLayout>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
