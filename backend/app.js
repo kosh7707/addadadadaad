39d56c46c6373a53eb73c70689c254cad1f805f9
kosh7707 <kosh7707@gmail.com>
2024-06-08 15:54:06 +0900

    cors 문제 수정

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require("morgan");
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const cors = require('cors');
 const passport = require('passport');
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 require("./passport")();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +26,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 const indexRouter = require('./routes');
 const authRouter = require('./routes/auth');
