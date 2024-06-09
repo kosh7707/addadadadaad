37f3606af85e507d8b55b3ba753647de71347b0e
llddang <bodyness@naver.com>
2024-06-09 14:41:01 +0900

    refact: mocks data 삭제

--- frontend/src/mocks/diary.ts
+++
@@ -1,8 +0,0 @@
-import { DiaryInfo } from '../types';
-
-export const DiaryInfoArray: DiaryInfo[] = [
-  { diary_id: 1, date: '06/01/24', emoji: '😄', title: 'title 1', content: 'content' },
-  { diary_id: 2, date: '06/02/24', emoji: '😄', title: 'title 2', content: 'content' },
-  { diary_id: 5, date: '06/05/24', emoji: '😄', title: 'title 3', content: 'content' },
-  { diary_id: 9, date: '06/09/24', emoji: '😄', title: 'title 4', content: 'content' },
-];
